64e0487eb07b766a8a3f01cf2bcac1d3212cbaea
Samiksha <samikshaajha@gmail.com>
2026-02-24 16:27:51 +0530

    calc

---
+++ cal.js
@@ -0,0 +1,13 @@
+var a=12,b=3
+document.write("a+b=");
+let result =a+b;
+document.write(result);
+document.write("a-b=");
+ result =a-b;
+document.write(result);
+document.write("a*b=");
+ result =a*b;
+document.write(result);
+document.write("a/b=");
+result =a/b;
+document.write(result);
